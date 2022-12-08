83e8a104341f23c4909e1d42ea0c0b96bf6eb0b6
TenDRILLL <sofyeer@gmail.com>
2022-12-08 23:21:34 +0200

    implement role

---
+++ src/structures/Role.ts
@@ -0,0 +1,30 @@
+import {APIRole} from "discord-api-types/v10";
+import {RoleTags} from "./RoleTags";
+
+export class Role {
+    public id: string;
+    public name: string;
+    public color: number;
+    public hoist: boolean;
+    public icon: string | null;
+    public unicodeEmoji: string | null;
+    public position: number;
+    public permissions: string;
+    public managed: boolean;
+    public mentionable: boolean;
+    public tags: RoleTags | null;
+
+    constructor(raw: APIRole) {
+        this.id = raw.id;
+        this.name = raw.name;
+        this.color = raw.color;
+        this.hoist = raw.hoist;
+        this.icon = raw.icon ?? null;
+        this.unicodeEmoji = raw.unicode_emoji ?? null;
+        this.position = raw.position;
+        this.permissions = raw.permissions;
+        this.managed = raw.managed;
+        this.mentionable = raw.mentionable
+        this.tags = raw.tags ? new RoleTags(raw.tags) : null;
+    }
+}
