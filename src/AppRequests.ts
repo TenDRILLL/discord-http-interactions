8b0133a0c965dbb77aa8762a981fab8194184d88
TenDRILLL <sofyeer@gmail.com>
2022-12-08 23:19:23 +0200

    add type

--- src/AppRequests.ts
+++ src/AppRequests.ts
@@ -1,6 +1,7 @@
 import { verifyKey } from "discord-interactions";
 import { Client } from "./Client";
-import { Interaction } from "./Interaction";
+import { Interaction } from "./structures/Interaction";
+import { APIInteraction } from "discord-api-types/v10";
 
 export class AppRequests {
     private client: Client;
@@ -9,23 +10,18 @@ export class AppRequests {
     }
 
     verifyDiscordRequest(){
-        return function (req, res, buf, encoding) {
-            const signature = req.get("X-Signature-Ed25519");
-            const timestamp = req.get("X-Signature-Timestamp");
-            const isValidRequest = verifyKey(buf, signature, timestamp, this.client.publicKey);
-            if (!isValidRequest) {
-                res.status(401).send("Bad request signature");
-            }
+        return (req, res, buf) => {
+            if (!verifyKey(buf, req.get("X-Signature-Ed25519"), req.get("X-Signature-Timestamp"), this.client.publicKey)) res.status(401).send("Bad request signature");
         };
     }
 
     listener(){
         this.client.app.post(this.client.endpoint, (req,res)=>{
-            const interaction = req.body;
+            const interaction: APIInteraction = req.body;
             if(interaction.type === 1){
-                return res.send({type: 1});
+                return res.send({type: 1}); //Reply to Discord's Ping.
             } else {
-                this.client.emit("interaction",new Interaction(interaction));
+                this.client.emit("interaction",new Interaction(interaction, this.client));
             }
         });
     }
