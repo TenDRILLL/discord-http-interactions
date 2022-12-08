1e0bee77253bf85cf4f5e4187fda6f2e6af281d5
TenDRILLL <sofyeer@gmail.com>
2022-12-08 23:20:46 +0200

    implement attachment

---
+++ src/structures/Attachment.ts
@@ -0,0 +1,27 @@
+import {APIAttachment} from "discord-api-types/v10";
+
+export class Attachment {
+    public id: string;
+    public filename: string;
+    public description: string | null;
+    public contentType: string | null;
+    public size: number;
+    public url: string;
+    public proxyUrl: string;
+    public height: number | null;
+    public width: number | null;
+    public ephemeral: boolean | null;
+
+    constructor(raw: APIAttachment) {
+        this.id = raw.id;
+        this.filename = raw.filename;
+        this.description = raw.description ?? null;
+        this.contentType = raw.content_type ?? null;
+        this.size = raw.size;
+        this.url = raw.url;
+        this.proxyUrl = raw.proxy_url;
+        this.height = raw.height ?? null;
+        this.width = raw.width ?? null;
+        this.ephemeral = raw.ephemeral ?? null;
+    }
+}
