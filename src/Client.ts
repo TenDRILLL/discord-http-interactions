37cff0056af5ad69db4b4c042ffa94c5efa31458
TenDRILLL <sofyeer@gmail.com>
2022-12-08 23:19:44 +0200

    Make token optional and implement rest

--- src/Client.ts
+++ src/Client.ts
@@ -1,34 +1,34 @@
 import {EventEmitter} from "events";
 import express from "express";
 import {AppRequests} from "./AppRequests";
+import {REST} from "@discordjs/rest";
 
 export class Client extends EventEmitter {
-    token: string;
-    applicationID: string;
-    publicKey: string;
-    port: number;
-    endpoint: string;
-    app;
-    requests;
+    private token: string;
+    public publicKey: string;
+    private port: number;
+    public endpoint: string;
+    public app;
+    private requests;
+    public rest;
 
     constructor(params){
         super();
         this._validateParams(params);
         this.token = params.token;
-        this.applicationID = params.applicationID;
         this.publicKey = params.publicKey;
         this.port = params.port;
         this.endpoint = params.endpoint;
         this.app = express();
         this.requests = new AppRequests(this);
+        this.rest = new REST({version: "10"}).setToken(this.token);
     }
 
     _validateParams(params){
-        if(!(params["token"])) throw new Error("NO_TOKEN_PROVIDED: No Token provided in the Client Constructor.");
-        if(!(params["applicationID"])) throw new Error("NO_APPLICATION_ID_PROVIDED: No Application ID provided in the Client Constructor.");
+        if(!(params["token"])) console.warn("NO_TOKEN_PROVIDED: No Token provided in the Client Constructor. This will restrict certain functionalities.");
         if(!(params["publicKey"])) throw new Error("NO_PUBLIC_KEY_PROVIDED: No Public Key provided in the Client Constructor.");
-        if(!(params["port"])) throw new Error("NO_PORT_PROVIDED: No Port provided in the Client Constructor.");
         if(!(params["endpoint"])) throw new Error("NO_ENDPOINT_PROVIDED: No Endpoint provided in the Client Constructor.");
+        if(!(params["port"])) throw new Error("NO_PORT_PROVIDED: No Port provided in the Client Constructor.");
     }
 
     login(){
