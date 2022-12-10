471bb48b482a2961d8d719c755203904600ae137
TenDRILLL <sofyeer@gmail.com>
2022-12-10 13:20:03 +0200

    implement reaction

---
+++ src/structures/Reaction.ts
@@ -0,0 +1,13 @@
+import {APIReaction} from "discord-api-types/v10";
+import {PartialEmoji} from "./PartialEmoji";
+
+export class Reaction {
+    public count: number;
+    public me: boolean;
+    public emoji: PartialEmoji;
+    constructor(raw: APIReaction) {
+        this.count = raw.count;
+        this.me = raw.me;
+        this.emoji = new PartialEmoji(raw.emoji);
+    }
+}
