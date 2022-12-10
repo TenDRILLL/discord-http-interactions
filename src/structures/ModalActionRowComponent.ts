b3713c578e715f9f8c5bc7858fb19e2bc284b2ca
TenDRILLL <sofyeer@gmail.com>
2022-12-10 13:19:39 +0200

    implement modalactionrowcomponent

---
+++ src/structures/ModalActionRowComponent.ts
@@ -0,0 +1,9 @@
+import {APIModalActionRowComponent} from "discord-api-types/v10";
+
+export class ModalActionRowComponent {
+    public type: number;
+
+    constructor(raw: APIModalActionRowComponent) {
+        this.type = raw.type;
+    }
+}
