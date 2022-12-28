89249b8d584a7ea7c4b276cc522b9ea6cb56eecd
TenDRILLL <sofyeer@gmail.com>
2022-12-28 23:14:10 +0200

    autocomplete should also include name and data

--- src/structures/ApplicationCommandAutocompleteInteraction.ts
+++ src/structures/ApplicationCommandAutocompleteInteraction.ts
@@ -1,12 +1,15 @@
 import {Interaction} from "./Interaction";
 import {APIApplicationCommandAutocompleteInteraction} from "discord-api-types/v10";
 import Client from "../Client";
+import {ApplicationCommandInteractionData} from "./ApplicationCommandInteraction";
 
 export class ApplicationCommandAutocompleteInteraction extends Interaction{
-    public data: null;
+    public data: ApplicationCommandInteractionData;
+    public commandName: string;
 
     constructor(raw: APIApplicationCommandAutocompleteInteraction, client: Client) {
         super(raw, client);
-        this.data = null;
+        this.data = new ApplicationCommandInteractionData(raw.data);
+        this.commandName = this.data.name;
     }
 }
